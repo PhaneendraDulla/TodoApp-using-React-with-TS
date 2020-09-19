9e483b7675380b265ba1b45ff5421d7b33b3d59d
Phaneendra Dulla <44269412+PhaneendraDulla@users.noreply.github.com>
2020-09-19 17:06:56 -0400

    Update TodoList.tsx

--- src/Components/TodoList.tsx
+++ src/Components/TodoList.tsx
@@ -12,7 +12,7 @@ const TodoList: React.FC<TodoListProps> = (props) => {
       {props.items.map((todo) => (
         <li key={todo.id}>
           <span>{todo.text}</span>
-          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>DELETE</button>
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>DONE</button>
         </li>
       ))}
     </ul>
